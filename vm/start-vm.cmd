e4a7506b935286847e55092a3276cc8c104a9a73
Pat Raynor <raynorpat@raynorpat.com>
2026-07-24 21:40:56 -0400

    vm: remove old qemu and begin use of modern qemu

--- vm/start-vm.cmd
+++ vm/start-vm.cmd
@@ -2,29 +2,10 @@
 setlocal
 cd /d "%~dp0"
 
-if not exist "qemu.exe" (
-  echo start-vm: qemu.exe not found in %CD%
-  exit /b 1
-)
-if not exist "rhapsody.vmdk" (
-  echo start-vm: rhapsody.vmdk not found in %CD%
-  exit /b 1
-)
-if not exist "pc-bios" (
-  echo start-vm: pc-bios directory not found in %CD%
-  exit /b 1
-)
-
-echo Starting Rhapsody QEMU ^(ne2k_pci + UDP socket 5001-^>5000^)...
-echo Ensure start-hecnet.cmd is already running.
+echo Launching QEMU...
 echo.
 
-qemu -L pc-bios -m 512 ^
-  -k en-us ^
-  -rhapsodymouse ^
-  -hda rhapsody.vmdk ^
-  -net nic,model=ne2k_pci,vlan=5 ^
-  -net socket,udp=0.0.0.0:5001,remote=127.0.0.1:5000 ^
-  -boot c
+qemu-system-i386 -M pc -cpu pentium -accel tcg -m 128 -k en-us -vga cirrus -hda rhapsody.vmdk -net nic,model=ne2k_pci -net user -boot order=c
 
+echo.
 exit /b %ERRORLEVEL%

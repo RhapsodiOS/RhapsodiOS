b42808db2d4e7ced7012660dacd06ee65c5ccf23
Pat Raynor <raynorpat@raynorpat.com>
2026-07-27 01:47:22 -0400

    drvCirrusLogicGD5434: write the display driver class from the reference disassembly
    Replaces the invented IOPCIDirectDevice subclass with Apple's IOFrameBufferDisplay one.

--- src/drivers-i386/video/drvCirrusLogicGD5434/CirrusLogicGD5434.drvproj/CirrusLogicGD5434DisplayDriver.lksproj/CirrusLogicGD5434DisplayDriver.h
+++ src/drivers-i386/video/drvCirrusLogicGD5434/CirrusLogicGD5434.drvproj/CirrusLogicGD5434DisplayDriver.lksproj/CirrusLogicGD5434DisplayDriver.h
@@ -1,157 +1,103 @@
-/*
- * CirrusLogicGD5434DisplayDriver.h
- * Cirrus Logic GD5434 Display Driver
+/* Copyright (c) 1996-1998 by NeXT Software, Inc. as an unpublished work.
+ * All rights reserved.
+ *
+ * CirrusLogicGD5434DisplayDriver.h -- interface for the Cirrus Logic
+ * CL-GD543X and CL-GD5446 display driver.
  */
 
-#import <driverkit/i386/IOPCIDirectDevice.h>
-#import <driverkit/IODevice.h>
+#ifndef CIRRUSLOGICGD5434DISPLAYDRIVER_H__
+#define CIRRUSLOGICGD5434DISPLAYDRIVER_H__
 
-// GD5434 PCI IDs
-#define CIRRUS_VENDOR_ID    0x1013
-#define GD5434_DEVICE_ID    0x00A8
+#import <driverkit/IOFrameBufferDisplay.h>
 
-// Memory ranges
-#define VGA_MEMORY_BASE     0xa0000
-#define VGA_MEMORY_SIZE     0x20000
-#define EXT_MEMORY_BASE     0xc0000
-#define EXT_MEMORY_SIZE     0x10000
-#define FRAMEBUFFER_BASE    0x00000000
-#define FRAMEBUFFER_SIZE    0x05000000
+/* The register values that program one display mode. The `parameters'
+ * field of every `IODisplayInfo' in the mode tables points at one of
+ * these. The four arrays are the standard VGA register files; the ten
+ * scalars that follow are the Cirrus extensions.
+ */
 
-// I/O Ports
-#define CRT_INDEX_PORT      0x3D4
-#define CRT_DATA_PORT       0x3D5
-#define SEQ_INDEX_PORT      0x3C4
-#define SEQ_DATA_PORT       0x3C5
-#define GFX_INDEX_PORT      0x3CE
-#define GFX_DATA_PORT       0x3CF
-#define ATTR_INDEX_PORT     0x3C0
-#define ATTR_DATA_PORT      0x3C1
-#define INPUT_STATUS_PORT   0x3DA
-#define MISC_OUTPUT_PORT    0x3C2
-#define DAC_WRITE_PORT      0x3C8
-#define DAC_DATA_PORT       0x3C9
-
-// CRTC Registers
-#define CRTC_HTOTAL         0x00
-#define CRTC_HDISP_END      0x01
-#define CRTC_HBLANK_START   0x02
-#define CRTC_HBLANK_END     0x03
-#define CRTC_HSYNC_START    0x04
-#define CRTC_HSYNC_END      0x05
-#define CRTC_VTOTAL         0x06
-#define CRTC_OVERFLOW       0x07
-#define CRTC_PRESET_ROW     0x08
-#define CRTC_MAX_SCAN       0x09
-#define CRTC_VDISP_END      0x12
-#define CRTC_OFFSET         0x13
-#define CRTC_VBLANK_START   0x15
-#define CRTC_VBLANK_END     0x16
-#define CRTC_MODE_CTRL      0x17
-#define CRTC_LINE_COMPARE   0x18
-
-// Extended CRTC Registers (Cirrus specific)
-#define CRTC_EXT_DISP       0x1B
-#define CRTC_EXT_OFFSET     0x1D
-
-// Sequencer Registers
-#define SEQ_RESET           0x00
-#define SEQ_CLOCKING_MODE   0x01
-#define SEQ_MAP_MASK        0x02
-#define SEQ_CHAR_MAP_SEL    0x03
-#define SEQ_MEMORY_MODE     0x04
-#define SEQ_EXT_MODE        0x07
-
-// Graphics Registers
-#define GFX_SET_RESET       0x00
-#define GFX_ENABLE_SET_RST  0x01
-#define GFX_COLOR_COMPARE   0x02
-#define GFX_DATA_ROTATE     0x03
-#define GFX_READ_MAP_SEL    0x04
-#define GFX_MODE            0x05
-#define GFX_MISC            0x06
-#define GFX_COLOR_DONT_CARE 0x07
-#define GFX_BIT_MASK        0x08
-
-// Attribute Registers
-#define ATTR_PALETTE_BASE   0x00
-#define ATTR_MODE_CTRL      0x10
-#define ATTR_OVERSCAN       0x11
-#define ATTR_COLOR_PLANE_EN 0x12
-#define ATTR_HORIZ_PEL_PAN  0x13
-#define ATTR_COLOR_SELECT   0x14
-
-// Display modes
 typedef struct {
-    int width;
-    int height;
-    int bitsPerPixel;
-    int refreshRate;
-    const char *name;
-} DisplayMode;
+    unsigned char misc;			/* Miscellaneous output (3C2) */
+    unsigned char seq[4];		/* SR01 - SR04 */
+    unsigned char crtc[25];		/* CR00 - CR18 */
+    unsigned char attr[21];		/* AR00 - AR14 */
+    unsigned char gfx[9];		/* GR00 - GR08 */
+    unsigned char seq07;		/* SR07 */
+    unsigned char seq0F;		/* SR0F, merged under mask 0x9F */
+    unsigned char seq0E;		/* SR0E; zero means a plain VGA mode */
+    unsigned char seq1E;		/* SR1E */
+    unsigned char gfx0B;		/* GR0B, merged under mask 0xC0 */
+    unsigned char crtc1A;		/* CR1A */
+    unsigned char crtc1B;		/* CR1B */
+    unsigned char seq16WhenSet;		/* SR16, SR0F bit 2 set */
+    unsigned char seq16WhenClear;	/* SR16, SR0F bit 2 clear */
+    unsigned char hiddenDAC;		/* Cirrus hidden DAC (3C6) */
+} GD5434Mode;
 
-@interface CirrusLogicGD5434DisplayDriver : IOPCIDirectDevice
+@interface CirrusLogicGD5434DisplayDriver : IOFrameBufferDisplay
 {
-    IORange vgaMemRange;
-    IORange extensionMemRange1;
-    IORange extensionMemRange2;
-    IORange portRange1;
-    IORange portRange2;
-    IORange portRange3;
-    void *mappedVGAMem;
-    void *mappedExtMem1;
-    void *mappedExtMem2;
+    /* The memory installed on this device. */
+    unsigned int installedVRAMBytes;
 
-    int currentWidth;
-    int currentHeight;
-    int currentBPP;
-    int currentRefresh;
-    unsigned char *framebuffer;
-    unsigned int framebufferSize;
+    unsigned int pciBus;
+    unsigned int pciAddress;
+
+    /* Nonzero if the config table says this is a PCI device. */
+    int busType;
+
+    /* The physical address of the frame buffer. */
+    unsigned int physicalAddress;
+
+    /* The mapped frame buffer. */
+    void *vram;
+
+    /* The transfer tables for this mode. */
+    unsigned char *redTransferTable;
+    unsigned char *greenTransferTable;
+    unsigned char *blueTransferTable;
+
+    /* The number of entries in the transfer table. */
+    int transferTableCount;
+
+    /* The current screen brightness. */
+    int brightnessLevel;
+
+    /* 0 while initializing, 1 in linear mode, 2 in VGA mode. */
+    int currentMode;
+
+    /* 0 GD5430, 1 GD5434, 2 GD5436, 3 GD5446, 4 unknown. */
+    int chipType;
+
+    /* The table of modes for this device, and its size. */
+    IODisplayInfo *modeTable;
+    int modeTableCount;
+
+    /* The mode used when the configured one is unavailable. */
+    int defaultMode;
 }
 
-// Class methods
-+ (BOOL)probe:(IOPCIDeviceDescription *)devDesc;
-
-// Instance methods
-- (BOOL)initFromDeviceDescription:(IOPCIDeviceDescription *)devDesc;
-- free;
-- (IOReturn)getIntValues:(unsigned *)paramArray
-             forParameter:(IOParameterName)parameterName
-                    count:(unsigned *)count;
-- (IOReturn)setIntValues:(unsigned *)paramArray
-             forParameter:(IOParameterName)parameterName
-                    count:(unsigned)count;
-
-// Initialization
-- (void)resetDevice;
-- (void)initializeHardware;
-- (void)setupPalette;
-
-// VGA/SVGA methods
+- initFromDeviceDescription:deviceDescription;
+- (int)selectMode;
 - (void)enterLinearMode;
 - (void)revertToVGAMode;
-- (void)setMode:(int)width height:(int)height bpp:(int)bpp refresh:(int)refresh;
+- (BOOL)determineConfiguration;
+- (BOOL)isValidPCIAssignedBaseAddress:(unsigned int)address;
+- (BOOL)setPCIConfiguration;
+- (void)setMode:(const GD5434Mode *)mode;
 - (void)clearScreen;
-- (void)displayModes;
-
-// Hardware access methods
-- (void)writeRegister:(unsigned short)reg value:(unsigned char)val;
-- (unsigned char)readRegister:(unsigned short)reg;
-- (void)writeCRTC:(unsigned char)index value:(unsigned char)val;
-- (unsigned char)readCRTC:(unsigned char)index;
-- (void)writeSequencer:(unsigned char)index value:(unsigned char)val;
-- (unsigned char)readSequencer:(unsigned char)index;
-- (void)writeGraphics:(unsigned char)index value:(unsigned char)val;
-- (unsigned char)readGraphics:(unsigned char)index;
-- (void)writeAttribute:(unsigned char)index value:(unsigned char)val;
-- (unsigned char)readAttribute:(unsigned char)index;
-
-// Framebuffer operations
-- (void)fillRect:(int)x y:(int)y width:(int)w height:(int)h color:(unsigned char)color;
-- (void)drawPixel:(int)x y:(int)y color:(unsigned char)color;
-- (unsigned char)getPixel:(int)x y:(int)y;
-- (void)copyRect:(int)srcX srcY:(int)srcY destX:(int)destX destY:(int)destY
-           width:(int)w height:(int)h;
+- (const char *)name;
+- (BOOL)setPendingDisplayMode:(int)mode;
+- (unsigned int)displayModeCount;
+- (IODisplayInfo *)displayModes;
+- (unsigned int)displayMemorySize;
+- (unsigned int)ramdacSpeed;
 
 @end
+
+@interface CirrusLogicGD5434DisplayDriver (ProgramDAC)
+- setTransferTable:(const unsigned int *)table count:(int)count;
+- setBrightness:(int)level token:(int)t;
+- setGammaTable;
+@end
+
+#endif	/* CIRRUSLOGICGD5434DISPLAYDRIVER_H__ */

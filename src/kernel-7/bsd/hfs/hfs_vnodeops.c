c7ecc61d1f319eb0939613f6dcb9d8bab093449b
Pat Raynor <raynorpat@raynorpat.com>
2026-09-23 19:57:19 -0400

    kernel: have searchfs return the error when an HFS Plus name won't convert, instead of packing it unterminated

--- src/kernel-7/bsd/hfs/hfs_vnodeops.c
+++ src/kernel-7/bsd/hfs/hfs_vnodeops.c
@@ -4530,9 +4530,9 @@ static int InsertMatch( struct vnode *root_vp, struct uio *a_uio, CatalogRecord 
 										 sizeof(catalogInfo.spec.name),
 										 &actualDstLen,
 										 catalogInfo.spec.name);
-			if ( err != noErr )
-				return( MacToVFSError(err) );	/* corrupt name: spec.name was not filled in */
 		}
+		if ( err != noErr )
+			return( MacToVFSError(err) );	/* spec.name was not filled in or not terminated */
 	}
 
 	PackCatalogInfoAttributeBlock( returnAttrList,root_vp,  &catalogInfo, &rovingAttributesBuffer, &rovingVariableBuffer );
